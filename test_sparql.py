d1b73a7adb82989820d268a2357cca4868afcd90
xuyangwang0825 <251005628@buaa.edu.cn>
2022-11-21 20:47:28 -0800

    Update test_sparql.py
    test get all properties

--- test_sparql.py
+++ test_sparql.py
@@ -1,4 +1,7 @@
 from rdflib import Graph
+from rdflib.plugins.sparql.results.jsonresults import JSONResultSerializer
+from collections import defaultdict
+import sys
 
 g = Graph().parse("src/hskg.ttl", format="turtle")
 q2 = """
@@ -10,3 +13,19 @@ q2 = """
     """
 for r in g.query(q2):
     print(r)
+
+# test get all properties
+g = Graph().parse("src/hskg.ttl", format="turtle")
+q2 = """
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        SELECT ?p ?property ?value WHERE { 
+            ?p foaf:name ?name .
+            ?p ?property ?value.
+        } 
+    """
+res = defaultdict(dict)
+for r in g.query(q2):
+    res[r.p.toPython()][r.property.toPython()] = r.value.toPython()
+    break
+
+print(res)
