fb49a105ee498a9891adaa0c3d68421dad1f14ca
xuyangwang0825 <251005628@buaa.edu.cn>
2022-11-23 19:21:56 -0800

    Update dataset for recommendation

--- pre-process/dataset_generator.py
+++ pre-process/dataset_generator.py
@@ -1,5 +1,6 @@
 import jsonlines
 import csv
+import json
 import random
 
 csvFile = open('../src/recommend_dataset.csv', 'w+')
@@ -16,23 +17,32 @@ with jsonlines.open('../src/desks.jsonl') as decks:
             csvWriter.writerow(tmp_data)
 
             # update card pool
-            card_pool.add(card)
+            # card_pool.add(card)
 
-with jsonlines.open('../src/desks.jsonl') as decks:
-    # 2nd round: generate not has card data
-    for deck in decks:
-        name = deck['name']
-        deck_cards_set = set(deck['cards'])
-        card_num = len(deck_cards_set)
-        tmp_pool = tuple(card_pool)
-        random_set = set()
-        while True:
-            random_card = random.choice(tmp_pool)
-            if random_card not in deck_cards_set and random_card not in random_set:
-                tmp_data = [name, "not_has_card", random_card]
-                csvWriter.writerow(tmp_data)
-                random_set.add(random_card)
-            # ratio of not_has_card data / has_card data is 3:1
-            if len(random_set) == 3 * card_num:
-                break
+cards = json.load(open('../src/cards.json'))
+for card in cards:
+    name = card['name']
+    properties = ['cardClass', 'cost', 'rarity', 'type']
+    for p in properties:
+        if p in card:
+            p_value = card[p]
+            csvWriter.writerow([name, f"{p}_is", p_value])
+
+# with jsonlines.open('../src/desks.jsonl') as decks:
+#     # 2nd round: generate not has card data
+#     for deck in decks:
+#         name = deck['name']
+#         deck_cards_set = set(deck['cards'])
+#         card_num = len(deck_cards_set)
+#         tmp_pool = tuple(card_pool)
+#         random_set = set()
+#         while True:
+#             random_card = random.choice(tmp_pool)
+#             if random_card not in deck_cards_set and random_card not in random_set:
+#                 tmp_data = [name, "not_has_card", random_card]
+#                 csvWriter.writerow(tmp_data)
+#                 random_set.add(random_card)
+#             # ratio of not_has_card data / has_card data is 3:1
+#             if len(random_set) == 3 * card_num:
+#                 break
 csvFile.close()
